bc7d78ad61ed0848a53b4f4fbe58689d7fd8d6a2
Kostas Milonas <milonas.ko@gmail.com>
2014-06-26 01:01:22 +0300

    adds pagination bundle

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd73f1ce3c8cd7da716f96303e06d0c95::getLoader();
+return ComposerAutoloaderInit6d93a76eb05061a5a7af21dcbe122a76::getLoader();
