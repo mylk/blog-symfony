070f157ef412536157e8522e8e29a983cda3f74d
Kostas Milonas <milonas.ko@gmail.com>
2014-09-25 21:04:02 +0300

    adds db delta with initial data

---
+++ _db/blog.init-data.sql
@@ -0,0 +1,95 @@
+-- MySQL dump 10.15  Distrib 10.0.13-MariaDB, for Linux (i686)
+--
+-- Host: localhost    Database: blog
+-- ------------------------------------------------------
+-- Server version	10.0.13-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (9,'Misc'),(10,'Really interesting');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (21,'Kostas','milonas.ko@gmail.com','Interesting fact...','2014-09-25 20:48:15',15,0),(22,'Kostas','milonas.ko@gmail.com','GO!','2014-09-25 20:49:12',14,0),(23,'Kostas','milonas.ko@gmail.com','Self control.','2014-09-25 20:54:36',14,0),(24,'Kostas','milonas.ko@gmail.com','Testing the mailer','2014-09-25 20:55:38',14,0);
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,NULL,'Home','homepage','route'),(2,NULL,'About','#','url'),(3,NULL,'RSS','rss','route'),(4,2,'Me','#','url');
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (14,'Welcome to my blog','This is my blog implemented on top of the Symfony2 framework.\r\n<br />\r\nThat\'s a sticky post.','2014-09-25 20:38:06',1,1,10,'2014-09-25 20:47:28',1,8),(15,'A post about flying cats','The undisputable truth is that cats can fly indeed.','2014-09-25 20:39:44',0,1,9,NULL,NULL,2),(16,'Yet another interesting post','If every star of the milky way was a grain of salt, we would fill an olympic sized pool.','2014-09-25 20:44:51',0,1,10,NULL,NULL,0);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `posts_tags`
+--
+
+LOCK TABLES `posts_tags` WRITE;
+/*!40000 ALTER TABLE `posts_tags` DISABLE KEYS */;
+INSERT INTO `posts_tags` VALUES (14,5),(14,6),(15,7),(16,7);
+/*!40000 ALTER TABLE `posts_tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (5,'PHP'),(6,'Symfony'),(7,'Life');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin@admin.com','2014-09-25 10:14:35');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-25 21:02:27
