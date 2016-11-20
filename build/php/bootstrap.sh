18da123f63c032898fb7e65669584882ca898110
Kostas Milonas <milonas.ko@gmail.com>
2016-11-20 19:00:45 +0200

    moves assets to bundle, installs them on container boot

--- build/php/bootstrap.sh
+++ build/php/bootstrap.sh
@@ -5,5 +5,6 @@ composer install --no-interaction
 app/console doctrine:schema:drop --force
 app/console doctrine:schema:create
 app/console doctrine:fixtures:load -n
+app/console assets:install --symlink
 
 php-fpm7.0
